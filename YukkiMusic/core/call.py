94c66e401283a24b8bdf94e22942bd94e8dcd3ff
Teamonix <unioxo@outlook.com>
2024-12-03 12:26:31 +0530

    Update call.py

--- YukkiMusic/core/call.py
+++ YukkiMusic/core/call.py
@@ -556,19 +556,21 @@ class Call(PyTgCalls):
                     db[chat_id][0]["markup"] = "stream"
 
     async def ping(self):
+    async def ping(self):
         pings = []
         if config.STRING1:
-            pings.append(await self.one.ping)
+            pings.append(self.one.ping)
         if config.STRING2:
-            pings.append(await self.two.ping)
+            pings.append(self.two.ping)
         if config.STRING3:
-            pings.append(await self.three.ping)
+            pings.append(self.three.ping)
         if config.STRING4:
-            pings.append(await self.four.ping)
+            pings.append(self.four.ping)
         if config.STRING5:
-            pings.append(await self.five.ping)
+            pings.append(self.five.ping)
         return str(round(sum(pings) / len(pings), 3))
 
+    
     async def start(self):
         LOGGER(__name__).info("Starting PyTgCalls Client\n")
         if config.STRING1:
@@ -585,29 +587,31 @@ class Call(PyTgCalls):
     async def decorators(self):
         from pytgcalls import filters
 
+        @self.one.on_kicked()
+        @self.two.on_kicked()
+        @self.three.on_kicked()
+        @self.four.on_kicked()
+        @self.five.on_kicked()
+        @self.one.on_closed_voice_chat()
+        @self.two.on_closed_voice_chat()
+        @self.three.on_closed_voice_chat()
+        @self.four.on_closed_voice_chat()
+        @self.five.on_closed_voice_chat()
+        @self.one.on_left()
+        @self.two.on_left()
+        @self.three.on_left()
+        @self.four.on_left()
+        @self.five.on_left()
+        async def stream_services_handler(_, chat_id: int):
+            await self.stop_stream(chat_id)
+
+        
+
         @self.one.on_update(filters.stream_end)
         @self.two.on_update(filters.stream_end)
         @self.three.on_update(filters.stream_end)
         @self.four.on_update(filters.stream_end)
         @self.five.on_update(filters.stream_end)
-        @self.one.on_update(ChatUpdate.Status.CLOSED_VOICE_CHAT)
-        @self.two.on_update(ChatUpdate.Status.CLOSED_VOICE_CHAT)
-        @self.three.on_update(ChatUpdate.Status.CLOSED_VOICE_CHAT)
-        @self.four.on_update(ChatUpdate.Status.CLOSED_VOICE_CHAT)
-        @self.five.on_update(ChatUpdate.Status.CLOSED_VOICE_CHAT)
-        @self.one.on_update(ChatUpdate.Status.LEFT_CALL)
-        @self.two.on_update(ChatUpdate.Status.LEFT_CALL)
-        @self.three.on_update(ChatUpdate.Status.LEFT_CALL)
-        @self.four.on_update(ChatUpdate.Status.LEFT_CALL)
-        @self.five.on_update(ChatUpdate.Status.LEFT_CALL)
-        async def stream_services_handler(_, chat_id: int):
-            await self.stop_stream(chat_id)
-
-        @self.one.on_update(ChatUpdate.Status.KICKED)
-        @self.two.on_update(ChatUpdate.Status.KICKED)
-        @self.three.on_update(ChatUpdate.Status.KICKED)
-        @self.four.on_update(ChatUpdate.Status.KICKED)
-        @self.five.on_update(ChatUpdate.Status.KICKED)
         async def stream_end_handler(client, update: Update):
             if not isinstance(update, StreamAudioEnded):
                 return
@@ -615,11 +619,11 @@ class Call(PyTgCalls):
 
         
 
-         @self.one.on_update(filters.stream_end)
-         @self.two.on_update(filters.stream_end)
-         @self.three.on_update(filters.stream_end)
-         @self.four.on_update(filters.stream_end)
-         @self.five.on_update(filters.stream_end)
+        @self.one.on_participants_change()
+        @self.two.on_participants_change()
+        @self.three.on_participants_change()
+        @self.four.on_participants_change()
+        @self.five.on_participants_change() 
         async def participants_change_handler(client, update: Update):
             if not isinstance(
                 update, GroupCallParticipant
